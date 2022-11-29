c18d3135fe6fb219ecf1cf1bd87028e58ebf1885
mikhail_malkov <mikhailmalkov9@gmail.com>
2022-11-29 17:29:40 -0500

    commited exercise36

---
+++ exercise36/Program.cs
@@ -0,0 +1,42 @@
+﻿// Задайте одномерный массив, заполненный случайными числами. Найдите сумму элементов, стоящих на нечётных позициях.
+// [3, 7, 23, 12] -> 19
+// [-4, -6, 89, 6] -> 0
+
+
+var array = FillArray(6);
+Print(array);
+Console.WriteLine();
+var count = SumLessZero(array);
+Console.WriteLine(count);
+
+int SumLessZero(int[] array)
+{
+    int sum = 0;
+    foreach(int num in array)
+    {
+        if(num < 0)
+        {
+            sum += num;
+        }
+    }
+    return sum;
+}
+
+
+int[] FillArray(int length)
+{
+    int[] array = new int[length];
+    for(int i = 0; i < length; i++)
+    {
+        array[i] = new Random().Next(-9, 10);
+    }
+    return array;
+}
+
+void Print(int[] array)
+{
+    foreach(var num in array)
+    {
+        Console.Write($"{num}, ");
+    }
+}
